1844f3f812f697ee66ed3d79a84fbad237ad7569
Fernando Baldrich <52502935+Fern1ck@users.noreply.github.com>
2023-07-21 04:23:52 -0300

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,164 @@
+<script lang="ts">
+	import { onMount } from 'svelte';	
+	let datos : {Temperatura_Celcius: number, Humedad: number, Fecha: string}[];
+	let dispositivos : string[]
+	let dispositivoSeleccionado : string	
+
+	async function fetchData() {
+      //fetch dispositivos
+      const res = await fetch(`${import.meta.env.VITE_SERVERHOST}/dispositivos`);
+      dispositivos = await res.json();  
+      //se inicializa el select al primer dispositivo 
+      dispositivoSeleccionado = dispositivos[0]
+      await fetchRegistros()
+    }
+
+    async function fetchRegistros() {
+      const res = await fetch(`${import.meta.env.VITE_SERVERHOST}/registros/${dispositivoSeleccionado}`);
+      datos = await res.json();
+      console.log("datos recibidos: ", datos.length)
+    }
+
+	onMount(() => {
+		fetchData().then(() => {
+			//const interval = setInterval(fetchRegistros, 2000);
+			//return () => clearInterval(interval);
+		})
+	});
+
+	let columns
+  	$:{
+		if(datos){
+
+			if(datos[0].Humedad != 0){
+				columns = ["Temperatura ºC", "Humedad %", "Fecha"]
+			}
+			else{
+				columns = ["Temperatura ºC", "Fecha"]
+			}
+		}
+	}
+</script>
+
+<svelte:head>
+	<title>TP Baldrich y Ramirez</title>
+	<meta name="robots" content="noindex nofollow" />
+	<html lang="en" />
+</svelte:head>
+
+<main>
+  <h1>Sistemas de Hardware para la Administracion</h1>
+  <h3>Trabajo Práctico - Fernando Baldrich y Thiago Ramirez</h3>
+
+  {#key datos}
+  <div class="card">
+    {#if !datos}
+      <p>Cargando...</p>
+    {:else if datos.length === 0}
+      <h2 id="temp">No hay datos aún</h2>
+    {:else}
+
+    <h2>Dispositivo seleccionado</h2>
+	<button on:click={() => fetchRegistros()}>Actualizar datos</button>
+    <select bind:value={dispositivoSeleccionado} on:change={() => fetchRegistros()}>
+      {#each dispositivos as dispositivo}
+        <option value={dispositivo}>
+          {dispositivo}
+        </option>
+      {/each}
+    </select>
+
+	<div id="tempDiv">
+		<h3 id="tempInfo">Temperatura</h3>
+		<h1 id="tempInfo">{datos[0].Temperatura_Celcius} ºC</h1>
+	</div>
+
+	{#if datos[0].Humedad != 0}
+	<div id="humedadDiv">
+		<h3 id="humedadInfo">Humedad</h3>
+		<h1 id="humedadInfo">{datos[0].Humedad + "%"}</h1>
+	</div>
+	{/if}
+    <h2>Registros históricos</h2>
+    <table>
+      <tr>
+        {#each columns as column}
+          <th>{column}</th>
+        {/each}
+      </tr>
+      
+      {#each datos as row}
+        <tr>
+          {#each Object.values(row) as cell}
+		  {#if cell != 0}
+            <td contenteditable="false">{cell}</td>
+			{/if}
+          {/each}
+        </tr>
+      {/each}
+    </table>
+    {/if}
+  </div>
+{/key}
+  
+</main>
+
+<style>
+	:global(body) {
+		background-image: url("circuitos.jpg");
+	}
+
+	#tempDiv, #humedadDiv {
+	  display: flex;
+	  flex-direction: column;
+	  justify-content: center;
+	  margin: 2rem;
+	}
+	#tempInfo {
+	  color: #0eafe9;
+	  margin: 0;
+	}
+
+	main {
+		background-color: #242424;
+	}
+	#humedadInfo {
+	  color: rgb(28, 222, 35);
+	  margin: 0;
+	}
+  
+	main {
+	  padding: 20px;
+	  max-width: 800px;
+	  margin: 0 auto;
+	}
+  
+	.card {
+	  border: 1px solid #ccc;
+	  border-radius: 4px;
+	  padding: 20px;
+	  margin-bottom: 20px;
+	}
+  
+	select {
+	  padding: 8px;
+	  font-size: 16px;
+	  margin-bottom: 20px;
+	}
+  
+	table {
+	  width: 100%;
+	  border: 1px solid #ccc;
+	  border-collapse: collapse;
+	}
+  
+	th, td {
+	  border: 1px solid black;
+	  padding: 8px;
+	}
+
+	th {
+		background-color: #4a4a4a;
+	  width: 25%;
+	}
+  </style>
