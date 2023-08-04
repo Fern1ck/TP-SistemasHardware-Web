f39007df242dea17f5f98f91579853377d45c55a
Fernando Baldrich <52502935+Fern1ck@users.noreply.github.com>
2023-08-04 05:20:42 -0300

    Changed the format of the table to show more data, added the download button for the json logs

--- src/App.svelte
+++ src/App.svelte
@@ -1,133 +1,142 @@
 <script lang="ts">
-	import imgUrl from './circuitos.jpg'
-	import { onMount } from 'svelte';	
-	let datos : {Temperatura_Celcius: number, Humedad: number, Fecha: string}[];
-	let dispositivos : string[]
-	let dispositivoSeleccionado : string	
-
-	async function fetchData(YaSeleccionado : string | null) {
-      //fetch dispositivos
-      const res = await fetch(`${import.meta.env.VITE_SERVERHOST}/dispositivos`);
-      dispositivos = await res.json();  
-      //se inicializa el select al primer dispositivo 
-      dispositivoSeleccionado = YaSeleccionado ?? dispositivos[0]
-      await fetchRegistros()
-    }
-
-    async function fetchRegistros() {
-      const res = await fetch(`${import.meta.env.VITE_SERVERHOST}/registros/${dispositivoSeleccionado}`);
-      datos = await res.json();
-      console.log("datos recibidos: ", datos.length)
-    }
-
-	onMount(() => {
-		document.body.style.backgroundImage = `url(${imgUrl})`;
-		fetchData(null).then(() => {
-			//const interval = setInterval(fetchRegistros, 2000);
-			//return () => clearInterval(interval);
-		})
-	});
-
-	let columns
-  	$:{
-		if(datos){
-
-			if(datos[0].Humedad != 0){
-				columns = ["Temperatura ºC", "Humedad %", "Fecha"]
-			}
-			else{
-				columns = ["Temperatura ºC", "Fecha"]
-			}
-		}
+	import imgUrl from './circuitos.jpg';
+	import { onMount } from 'svelte';
+	let datos: { Fecha: string, Dispositivo: string, Datos: {Variable: string, Valor: string}[] }[];
+	let dispositivos: string[];
+	let dispositivoSeleccionado: string;
+  
+	async function fetchData(YaSeleccionado: string | null) {
+	  //fetch dispositivos
+	  const res = await fetch(`${import.meta.env.VITE_SERVERHOST}/dispositivos`);
+	  dispositivos = await res.json();
+	  //se inicializa el select al primer dispositivo
+	  dispositivoSeleccionado = YaSeleccionado ?? dispositivos[0];
+	  await fetchRegistros();
 	}
-</script>
+  
+	async function fetchRegistros() {
+	  const res = await fetch(
+		`${import.meta.env.VITE_SERVERHOST}/registros/${dispositivoSeleccionado}`
+	  );
+	  datos = await res.json();	 
+	}
+  
+	async function descargarJson(){
+		const res = await fetch(
+		`${import.meta.env.VITE_SERVERHOST}/registrosJSON`
+	  	);
+		const blob = await res.blob()
+ 
+    	var file = window.URL.createObjectURL(blob);
 
-<svelte:head>
+  		const downloadLink = document.createElement('a');
+  		downloadLink.href = file;
+  		downloadLink.download = 'registros.json'; 
+			
+  		downloadLink.click();
+			
+  		window.URL.revokeObjectURL(file);
+  };
+	
+	onMount(() => {
+	  document.body.style.backgroundImage = `url(${imgUrl})`;
+	  fetchData(null)
+	});
+  
+	let columns = [] ;
+  
+	$: {
+	  if (datos) {
+		  columns = [ ...Object.keys(datos[0])];
+	  }
+	}
+  </script>
+  
+  <svelte:head>
 	<title>TP Baldrich y Ramirez</title>
 	<meta name="robots" content="noindex nofollow" />
 	<html lang="en" />
-</svelte:head>
-
-<main>
-  <h1>Sistemas de Hardware para la Administracion</h1>
-  <h3>Trabajo Práctico - Fernando Baldrich y Thiago Ramirez</h3>
-
-  {#key datos}
-  <div class="card">
-    {#if !datos}
-	<h2 id="tempInfo">Cargando...</h2>
-    {:else if datos.length === 0}
-	{console.log("datos: ", datos)}
-      <h2 id="tempInfo">No hay datos aún</h2>
-    {:else}
-
-    <h2>Dispositivo seleccionado: {dispositivoSeleccionado}</h2>
-	<button on:click={() => fetchData(dispositivoSeleccionado)}>Actualizar datos</button>
-    <select bind:value={dispositivoSeleccionado} on:change={() => fetchRegistros()}>
-      {#each dispositivos as dispositivo}
-        <option value={dispositivo}>
-          {dispositivo}
-        </option>
-      {/each}
-    </select>
-
-	<div id="tempDiv">
-		<h3 id="tempInfo">Temperatura</h3>
-		<h1 id="tempInfo">{datos[0].Temperatura_Celcius} ºC</h1>
-	</div>
-
-	{#if datos[0].Humedad != 0}
-	<div id="humedadDiv">
-		<h3 id="humedadInfo">Humedad</h3>
-		<h1 id="humedadInfo">{datos[0].Humedad + "%"}</h1>
-	</div>
-	{/if}
-    <h2>Registros históricos</h2>
-    <table>
-      <tr>
-        {#each columns as column}
-          <th>{column}</th>
-        {/each}
-      </tr>
-      
-      {#each datos as row}
-        <tr>
-          {#each Object.values(row) as cell}
-		  {#if cell != 0}
-            <td contenteditable="false">{cell}</td>
-			{/if}
-          {/each}
-        </tr>
-      {/each}
-    </table>
-    {/if}
-  </div>
-{/key}
+  </svelte:head>
   
-</main>
+  <main>
+	<h1>Sistemas de Hardware para la Administración</h1>
+	<h3>Trabajo Práctico - Fernando Baldrich y Thiago Ramirez</h3>
+  
+	{#key datos}
+	<div class="card">
+	  {#if !datos}
+	  	<h2 id="tempInfo">Cargando...</h2>
+	  	{:else if datos.length === 0}
+	  	<h2 id="tempInfo">No hay datos aún</h2>
+	  	{:else}
+		
+	  	<h2>Dispositivo seleccionado: {dispositivoSeleccionado}</h2>
+	  	<button on:click={() => fetchData(dispositivoSeleccionado)}>Actualizar datos</button>
+	  	<select bind:value={dispositivoSeleccionado} on:change={() => fetchRegistros()}>
+			{#each dispositivos as dispositivo}
+			<option value={dispositivo}>
+			  {dispositivo}
+			</option>
+			{/each}
+	  	</select>
+	  
+		<div id="gridContainer">
+			{#each datos[0].Datos as dato}
+				<div class="gridItem">
+					<h3 id="varName">{dato.Variable}</h3>
+					<h1 id="varValue">{dato.Valor}</h1>
+				</div>
+			{/each}
+		</div>
+		<h4 id="varDate">Datos recolectados el {datos[0].Fecha}</h4>
+		<button on:click={() => {descargarJson()}}>Descargar JSON</button>
+	  	<h2>Registros históricos</h2>
+	  	{#if datos.length > 0}
+		  <table>
+			<tr>
+			  {#each columns as column}
+				{#if column !== 'ID'} <!-- Exclude the 'ID' column -->
+				  <th>{column}</th>
+				{/if}
+			  {/each}
+			</tr>
+		  
+			{#each datos as row}
+			  <tr>
+				{#each Object.entries(row) as [key, value]}
+				  {#if key !== 'ID'} <!-- Exclude the 'ID' column -->
+					<td contenteditable="false">
+					  {@html Array.isArray(value) ? value.map(item => `<u>${item.Variable}</u>: ${item.Valor}`).join('<br/>') : value}
+					</td>
+				  {/if}
+				{/each}
+			  </tr>
+			{/each}
+		  </table>
+  		{/if}
+	  {/if}
+	</div>
+	{/key}
+  </main>
 
 <style>
 	* {
  		font-family: Arial;
 		color: white;
 	}
-	#tempDiv, #humedadDiv {
-	  display: flex;
-	  flex-direction: column;
+	#gridContainer{
 	  justify-content: center;
-	  margin: 2rem;
+	  display: grid;
+  		grid-template-columns: 200px 200px 200px;
+  		grid-gap: 30px;
 	}
-	#tempInfo {
+	#varValue {
 	  color: #0eafe9;
 	  margin: 0;
 	}
 
 	main {
 		background-color: #242424;
-	}
-	#humedadInfo {
-	  color: rgb(28, 222, 35);
-	  margin: 0;
 	}
   
 	main {
